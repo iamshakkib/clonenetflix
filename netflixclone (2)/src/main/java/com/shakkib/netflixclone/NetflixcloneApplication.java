19d326bc90ec6690e60785f64149dac66d3b5845
shakkib327559 <md2076379@gmail.com>
2022-01-23 18:45:03 +0530

    swagger open api added

--- netflixclone (2)/src/main/java/com/shakkib/netflixclone/NetflixcloneApplication.java
+++ netflixclone (2)/src/main/java/com/shakkib/netflixclone/NetflixcloneApplication.java
@@ -3,9 +3,11 @@ package com.shakkib.netflixclone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class NetflixcloneApplication {
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+@SpringBootApplication
+@OpenAPIDefinition
+public class NetflixcloneApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixcloneApplication.class, args);
 	}
